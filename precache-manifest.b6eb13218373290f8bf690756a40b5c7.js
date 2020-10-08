d6b4a5126a95a29d807a42969503b865bfb2905b
Khalil Abid <kabid@netisse.com>
2020-10-08 17:49:13 +0100

    first commit

---
+++ precache-manifest.b6eb13218373290f8bf690756a40b5c7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bcb0c42ef645b4f19d0d49537511728",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a605a971f5dc6667fe6",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9a48758de524d53f6e3c",
+    "url": "./static/css/main.c53e4c64.chunk.css"
+  },
+  {
+    "revision": "3a605a971f5dc6667fe6",
+    "url": "./static/js/2.3b0b857b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.3b0b857b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a48758de524d53f6e3c",
+    "url": "./static/js/main.1b37a2a5.chunk.js"
+  },
+  {
+    "revision": "8e71eb2661121c71b4e2",
+    "url": "./static/js/runtime-main.e16ce86d.js"
+  },
+  {
+    "revision": "baeaf0c7f02777839e58eed62cc714c4",
+    "url": "./static/media/loader.baeaf0c7.gif"
+  },
+  {
+    "revision": "300f4a6f859f8a579c350961e8414bb9",
+    "url": "./static/media/notfound.300f4a6f.png"
+  }
+]);
